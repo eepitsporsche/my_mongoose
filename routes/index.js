152251bd55ee5e6d3f26d817749fb87afd7ea47f
Porsche <herskorn@gmail.com>
2024-06-09 11:22:42 -0700

    created middleware for routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+//Import Express Router and Routes
+const router = require(express).Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+
+//Middleware for Routes
+router.use('/user', userRoutes);
+router.use('/thought', thoughtRoutes);
+
+module.exports = router;
